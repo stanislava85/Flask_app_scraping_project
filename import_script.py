11e343ec421b12bb36a0b632aeb6db36c404cccc
stanislava85 <stani85@gmail.com>
2020-12-16 21:48:05 -0500

    updating base.html

--- import_script.py
+++ import_script.py
@@ -6,17 +6,12 @@ mat_url = "https://worldpopulationreview.com/country-rankings/maternity-leave-by
 #passing the url link to the scraping script --> to be saved in a table
 mat_table = mat_leave(mat_url)
 
-# rows = { 
-#     "row1": "row1_val",
-#     "row2": "row2_val",
-# } 
 
-# this `main` function should run your scraping when 
-# this script is ran.
+# this `main` function runs the scraping when this script is ran.
 def main():
     db.drop_all()  #makes sure there are no tables and the database is empty
     db.create_all() #creates the columns defined inside the Class
-    for i in mat_table:  #yes girl!!! through the scraping results
+    for i in mat_table:  
         new_row = MaternityLeave(country=i[0], weeks_paid=i[1], payment_rate=i[2], population_2020=i[3]) #defining each column using undexing from the list results
         print(new_row)
         db.session.add(new_row)  #adds each row to the database
